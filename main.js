0d0c0cbfcc65aaa6dc3d24a450b7742ae8958c4c
Khadhraoui-Oussama <khadhraoui.oussama.dev@gmail.com>
2023-01-24 08:06:57 +0000

    fixed the copying function

--- main.js
+++ main.js
@@ -97,6 +97,32 @@ let spChars = [
   "/",
 ];
 //the copy functions
+function unsecuredCopyToClipboard(text) {
+  const textArea = document.createElement("textarea");
+  textArea.value = pass1;
+  document.body.appendChild(textArea);
+  textArea.focus();
+  textArea.select();
+  try {
+    document.execCommand("copy");
+  } catch (err) {
+    console.error("Unable to copy to clipboard", err);
+  }
+  document.body.removeChild(textArea);
+}
+
+/**
+ * Copies the text passed as param to the system clipboard
+ * Check if using HTTPS and navigator.clipboard is available
+ * Then uses standard clipboard API, otherwise uses fallback
+ */
+const copyToClipboard = (content) => {
+  if (window.isSecureContext && navigator.clipboard) {
+    navigator.clipboard.writeText(content);
+  } else {
+    unsecuredCopyToClipboard(content);
+  }
+};
 function copyPass1() {
   var copyText1 = document.getElementById("password1");
   copyText1.select;
